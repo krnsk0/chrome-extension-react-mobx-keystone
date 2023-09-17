b76465687e92650fe163f4db8db5861d276a6c22
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-09-17 15:44:11 -0400

    stub test

--- src/common/store/startStoreSync.spec.ts
+++ src/common/store/startStoreSync.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 
 describe('The startStoreSync function', () => {
-  it('should fail', () => {
-    expect(true).toBe(false);
+  it('should pass', () => {
+    expect(true).toBe(true);
   });
 });
