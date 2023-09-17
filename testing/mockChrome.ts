319af48eb11bc9289bac083c3522bb26dae4aa20
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-09-17 17:28:30 -0400

    make chrome mock actually functional

--- testing/mockChrome.ts
+++ testing/mockChrome.ts
@@ -1,19 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { vi } from 'vitest';
 
 /**
- * A partial mock of the chrome API for testing purposes
+ * A functional mock of the chrome API for testing purposes. All functions
+ * are spied for inspection
  */
-export const mockChrome = {
-  storage: {
-    local: {
-      get: vi.fn(),
-      set: vi.fn(),
+export const makeMockChrome = () => {
+  let dataStore: any = {};
+  let listeners: any[] = [];
+
+  return {
+    storage: {
+      local: {
+        get: vi.fn().mockImplementation((keys: string) => {
+          const result = { [keys]: dataStore[keys] };
+          return result;
+        }),
+        set: vi.fn().mockImplementation((data: Record<string, any>) => {
+          dataStore = { ...dataStore, ...data };
+        }),
+      },
+      onChanged: {
+        addListener: vi
+          .fn()
+          .mockImplementation(
+            (
+              listener: (changes: Record<string, any>, areaName: string) => void
+            ) => {
+              listeners.push(listener);
+            }
+          ),
+
+        removeListener: vi
+          .fn()
+          .mockImplementation(
+            (
+              listener: (changes: Record<string, any>, areaName: string) => void
+            ) => {
+              const index = listeners.indexOf(listener);
+              if (index !== -1) {
+                listeners.splice(index, 1);
+              }
+            }
+          ),
+      },
     },
-    onChanged: {
-      addListener: vi.fn(),
+
+    reset: function () {
+      dataStore = {};
+      listeners = [];
     },
-    removeListener: {
-      addListener: vi.fn(),
-    },
-  },
+  };
 };
--- testing/setup.ts
+++ testing/setup.ts
@@ -3,14 +3,16 @@ import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
-import { mockChrome } from './mockChrome';
+import { makeMockChrome } from './mockChrome';
 
 // stub the chrome API globally as it is not
 // part of JSDOM
+const mockChrome = makeMockChrome();
 vi.stubGlobal('chrome', mockChrome);
 
 // clear JSDOM state after each test
 afterEach(() => {
   cleanup();
   vi.resetAllMocks();
+  mockChrome.reset();
 });
