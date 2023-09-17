760edf72cc042302d0da79cce62a1d882b66baf5
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-09-17 13:24:42 -0400

    test fails

--- src/common/store/storeProvider.spec.tsx
+++ src/common/store/storeProvider.spec.tsx
@@ -2,12 +2,7 @@ import { describe, it, expect } from 'vitest';
 import StoreProvider from './storeProvider';
 import { render } from '@testing-library/react';
 
-
-
-
 describe('the StoreProvide', () => {
-
-
   it('should pass an instance of the store down to child components', () => {
     const Child = () => <></>;
     const { container } = render(
